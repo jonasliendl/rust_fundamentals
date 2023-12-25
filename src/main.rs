6a92eba8a8c7847c5531c12f4cdf6f6f81c91c3c
jonasliendl <57270170+jonasliendl@users.noreply.github.com>
2023-12-25 15:59:09 +0100

    🚀

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,51 @@
-#[derive(Debug)]
-struct Person {
-    first_name: String,
-    last_name: String,
-    age: u8,
+struct User {
+    username: String,
     email: String,
-    phone_number: String,
+    uri: String,
+    active: bool,
 }
 
-fn full_name(person: &Person) -> String {
-    format!("{} {}", person.first_name, person.last_name)
+impl User {
+    fn new(username: String, email: String, uri: String) -> Self {
+        Self {
+            username,
+            email,
+            uri,
+            active: true,
+        }
+    }
+    fn deactivate(&mut self) {
+        self.active = false;
+    }
+
+    fn from_email(email: String) -> Self {
+        let list: Vec<&str> = email.split('@').collect();
+        Self {
+            username: list[0].to_string(),
+            email: email.to_string(),
+            uri: list[1].to_string(),
+            active: true,
+        }
+    }
+
+    fn update_uri(&mut self, uri: String) {
+        self.uri = uri;
+    }
+
 }
 
 fn main() {
-    println!("Week 3 of Rust Fundamentals - Labor 01");
-    let person = &Person {
-        first_name: "John".to_string(),
-        last_name: "Doe".to_string(),
-        age: 25,
-        email: "example@example.de".to_string(),
-        phone_number: "+49123456789".to_string(),
-    };
-
-    println!("{}", full_name(&person));
+    let mut new_user = User::new(
+        String::from("alfredodeza"),
+        String::from("alfreodeza@example.com"),
+        String::from("https://alfredodeza.com"),
+    );
+    println!("Hello, {}!", new_user.username);
+    println!("Account {} status is: {}", new_user.username, new_user.active);
+    new_user.deactivate();
+    println!("Account {} status is: {}", new_user.username, new_user.active);
+    let mut user_from_email = User::from_email(String::from("john@example.com"));
+    println!("Hello, {}! uri: {}", user_from_email.username, user_from_email.uri);
+    user_from_email.update_uri(String::from("https://google.com"));
+    println!("uri: {}", user_from_email.uri);
 }
