c9656dee67709c15075db7f208c6179d414ea087
jonasliendl <57270170+jonasliendl@users.noreply.github.com>
2023-12-25 02:57:53 +0100

    solved

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#[derive(Debug)]
+struct Person {
+    first_name: String,
+    last_name: String,
+    age: u8,
+    email: String,
+    phone_number: String,
+}
+
+fn full_name(person: &Person) -> String {
+    format!("{} {}", person.first_name, person.last_name)
+}
+
 fn main() {
-    println!("Week 3 of Rust Fundamentals");
+    println!("Week 3 of Rust Fundamentals - Labor 01");
+    let person = &Person {
+        first_name: "John".to_string(),
+        last_name: "Doe".to_string(),
+        age: 25,
+        email: "example@example.de".to_string(),
+        phone_number: "+49123456789".to_string(),
+    };
+
+    println!("{}", full_name(&person));
 }
